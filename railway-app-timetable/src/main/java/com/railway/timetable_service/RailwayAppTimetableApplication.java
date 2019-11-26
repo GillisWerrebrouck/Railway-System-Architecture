0955c6ff569d1271037e1b7630c372465f22f459
Gillis Werrebrouck <gillis-wer@hotmail.be>
2019-11-26 13:18:26 +0100

    Create Timetable Service

---
+++ railway-app-timetable/src/main/java/com/railway/timetable_service/RailwayAppTimetableApplication.java
@@ -0,0 +1,13 @@
+package com.railway.timetable_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayAppTimetableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayAppTimetableApplication.class, args);
+	}
+
+}
