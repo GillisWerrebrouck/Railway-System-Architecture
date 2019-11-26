b5135a50c48f38596cca0c2e3d9e482c8ac6d77b
Gillis Werrebrouck <gillis-wer@hotmail.be>
2019-11-26 13:17:44 +0100

    Create Station Service

---
+++ railway-app-station/src/main/java/com/railway/station_service/RailwayAppStationApplication.java
@@ -0,0 +1,13 @@
+package com.railway.station_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayAppStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayAppStationApplication.class, args);
+	}
+
+}
