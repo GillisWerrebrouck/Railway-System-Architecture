a33a4f29fbbda736d65d4b919166ddfb18dfe190
Gillis Werrebrouck <gillis-wer@hotmail.be>
2019-11-26 13:17:16 +0100

    Create Route Management Service

---
+++ railway-app-route-management/src/main/java/com/railway/route_management_service/RailwayAppRouteManagementApplication.java
@@ -0,0 +1,13 @@
+package com.railway.route_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayAppRouteManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayAppRouteManagementApplication.class, args);
+	}
+
+}
