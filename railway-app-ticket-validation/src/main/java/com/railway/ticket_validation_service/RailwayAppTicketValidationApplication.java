3733f991f3e647bd1d2372101096e9a7e4253753
Gillis Werrebrouck <gillis-wer@hotmail.be>
2019-11-26 13:18:12 +0100

    Create Ticket Validation Service

---
+++ railway-app-ticket-validation/src/main/java/com/railway/ticket_validation_service/RailwayAppTicketValidationApplication.java
@@ -0,0 +1,13 @@
+package com.railway.ticket_validation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayAppTicketValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayAppTicketValidationApplication.class, args);
+	}
+
+}
