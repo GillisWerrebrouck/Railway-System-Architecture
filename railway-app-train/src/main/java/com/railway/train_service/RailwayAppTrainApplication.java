ab8fa6bbdfb34adb1a3aa46c52fca3cdb31d40aa
Elise <elise.thienpont@Ugent.be>
2019-11-29 21:37:46 +0100

    mongodb embedded

--- railway-app-train/src/main/java/com/railway/train_service/RailwayAppTrainApplication.java
+++ railway-app-train/src/main/java/com/railway/train_service/RailwayAppTrainApplication.java
@@ -1,5 +1,9 @@
 package com.railway.train_service;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -9,6 +13,7 @@ import org.springframework.context.annotation.Bean;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase; 
 
 @SpringBootApplication
@@ -23,18 +28,32 @@ public class RailwayAppTrainApplication {
 	@Bean
 	public CommandLineRunner testRepo(TrainRepository trainRepository) {
 		return (args) -> {
-			MongoClientURI uri = new MongoClientURI(
+			/*MongoClientURI uri = new MongoClientURI(
 				    "mongodb+srv://TrainService:panda@cluster0-6ipq1.mongodb.net/test?retryWrites=true&w=majority");
 
 				MongoClient mongoClient = new MongoClient(uri);
-				MongoDatabase database = mongoClient.getDatabase("test");
-				/*
+				MongoDatabase database = mongoClient.getDatabase("trainService");
+				MongoCollection c = database.getCollection("trains");
+				c*/
+				
 			logger.info("startBegin");
-			Train treintje = new Train(Type.IC, 9.999, 100, 15, Status.ACTIVE, null);
+			Map<String,String> m = new HashMap<String, String>();
+			m.put("kapotte deur", "da wordt een nieuwe deur");
+			TechnicalDetails t = new TechnicalDetails(Fuel.DIESEL, LocalDate.parse("2019-01-01"), m);
+			
+			Train treintje = new Train(Type.IC, 9.999, 100, 15, Status.ACTIVE, t);
 			trainRepository.save(treintje);
-			trainRepository.findAll().forEach((t) -> System.out.println(t.toString()));
-			System.out.println("start");*/
+			trainRepository.findAll().forEach((te) -> System.out.println(te.toString()));
+			System.out.println("start");
 		};
 	}
+	
+	/*
+	 * spring.data.mongodb.host=[host]
+spring.data.mongodb.port=[port]
+spring.data.mongodb.authentication-database=[authentication_database]
+spring.data.mongodb.username=[username]
+spring.data.mongodb.password=[password]
+spring.data.mongodb.database=rest_tutorial*/
 
 }
--- railway-app-train/src/main/java/com/railway/train_service/Train.java
+++ railway-app-train/src/main/java/com/railway/train_service/Train.java
@@ -1,6 +1,7 @@
 package com.railway.train_service;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 enum Type {IC, IR, P}
