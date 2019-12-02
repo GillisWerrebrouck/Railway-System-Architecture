802b49f95bec0c5dea92b216b9e530dfc8c5dae7
Tim Steels <Tim.Steels@ugent.be>
2019-12-02 13:23:43 +0100

    cleanup

--- railway-app-train/src/main/java/com/railway/train_service/RailwayAppTrainApplication.java
+++ railway-app-train/src/main/java/com/railway/train_service/RailwayAppTrainApplication.java
@@ -30,15 +30,7 @@ public class RailwayAppTrainApplication {
 	
 	@Bean
 	public CommandLineRunner testRepo(TrainRepository trainRepository) {
-		return (args) -> {
-			/*MongoClientURI uri = new MongoClientURI(
-		    "mongodb+srv://TrainService:panda@cluster0-6ipq1.mongodb.net/test?retryWrites=true&w=majority");
-
-		MongoClient mongoClient = new MongoClient(uri);
-		MongoDatabase database = mongoClient.getDatabase("trainService");
-		MongoCollection c = database.getCollection("trains");
-		c*/
-				
+		return (args) -> {	
 			Map<String,String> m = new HashMap<String, String>();
 			m.put("kapotte deur", "dat wordt een nieuwe deur");
 			TechnicalDetails t = new TechnicalDetails(FuelType.DIESEL, LocalDate.parse("2019-01-01"), m);
@@ -49,13 +41,6 @@ public class RailwayAppTrainApplication {
 			trainRepository.findAll().forEach((te) -> logger.info(te.toString()));
 		};
 	}
-	
-	/*
-	 * spring.data.mongodb.host=[host]
-spring.data.mongodb.port=[port]
-spring.data.mongodb.authentication-database=[authentication_database]
-spring.data.mongodb.username=[username]
-spring.data.mongodb.password=[password]
-spring.data.mongodb.database=rest_tutorial*/
+
 
 }
